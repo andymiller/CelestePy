cf4775c5f9264f353305663e7ceed13cdf74e71e
Andrew Miller <andrew.colin.miller@gmail.com>
2016-07-29 16:09:23 -0700

    cached unit psf image for color gibbs steps

--- examples/synthetic_gal.py
+++ examples/synthetic_gal.py
@@ -6,10 +6,11 @@ import matplotlib.pyplot as plt; plt.ion()
 import seaborn as sns; sns.set_style("white")
 import autograd.numpy as np
 from autograd import grad, hessian
-from CelestePy.point_source import make_gen_model_image, \
-                                   make_lnpdf_fun, bands
+from CelestePy.galaxy import make_gen_model_image, \
+                             make_lnpdf_fun, bands
 import synthetic_constants as sc
-from CelestePy.util.transform import fluxes_to_colors
+from CelestePy.util.transform import fluxes_to_colors, unconstrain_gal_shape, \
+                                     constrain_gal_shape
 
 
 def plot_chain_marginals(th_samps, true_params):
@@ -27,17 +28,17 @@ def plot_chain_marginals(th_samps, true_params):
     return fig, axarr
 
 
-def plot_pairwise(th_samps, true_params):
-    import pandas as pd
-    samp_df = pd.DataFrame(np.hstack([th_samps[len(th_samps)/2:,:5],
-                                      100*th_samps[len(th_samps)/2:, 5:7]]),
-                           columns=['lnr', 'cu', 'cg', 'cr', 'ci', 'ra', 'dec'])
-    pplot = sns.pairplot(samp_df, size=1.5)
-    tp_scaled = true_params.copy()
-    tp_scaled[5:7] *= 100.
-    for tp, ax in zip(tp_scaled, pplot.diag_axes):
-        ax.scatter(tp, 0, c='red', marker='x', s=50)
-
+#def plot_pairwise(th_samps, true_params):
+#    import pandas as pd
+#    samp_df = pd.DataFrame(np.hstack([th_samps[len(th_samps)/2:,:5],
+#                                      100*th_samps[len(th_samps)/2:, 5:7]]),
+#                           columns=['lnr', 'cu', 'cg', 'cr', 'ci', 'ra', 'dec'])
+#    pplot = sns.pairplot(samp_df, size=1.5)
+#    tp_scaled = true_params.copy()
+#    tp_scaled[5:7] *= 100.
+#    for tp, ax in zip(tp_scaled, pplot.diag_axes):
+#        ax.scatter(tp, 0, c='red', marker='x', s=50)
+#
 
 if __name__=="__main__":
 
@@ -51,8 +52,16 @@ if __name__=="__main__":
                  'u': 1.2582444245272928,
                  'z': 40.854689375715807}
     eps_dict  = {'u': 28., 'g': 307., 'r': 684., 'i': 817, 'z': 484.}
-    true_colors = fluxes_to_colors(np.array([flux_dict[b] for b in bands]))
-    true_params = np.concatenate([true_colors, u])
+    eps_dict  = {b: eps_dict[b] / 10. for b in bands}
+    true_shape= np.array([ .5, # theta_s
+                           36., # sig2_s (in pixels)
+                          45., # phi, north rotation [0, 180]
+                           .2  # rho min / major axis ratio
+                         ])
+    true_colors = np.array([1.80342137, -2.00360455, -1.59958647, -0.47531305,  0.34646993])
+    #true_colors = fluxes_to_colors(np.array([flux_dict[b] for b in bands]))
+    true_params = np.concatenate([true_colors, u,
+                                  unconstrain_gal_shape(true_shape)])
 
     ###################################################################
     # create model image generating functions, specific to the image  #
@@ -91,14 +100,14 @@ if __name__=="__main__":
                 for b in bands }
 
     # plot observations
-    from CelestePy.util.plots import add_colorbar_to_axis
-    fig, axarr = plt.subplots(1, 5, figsize=(16,4))
-    for b, ax in zip(bands, axarr.flatten()):
-        cim = ax.imshow(imgdict[b], interpolation='none')
-        add_colorbar_to_axis(ax, cim)
-        ax.set_title("band %s"%b)
-    fig.tight_layout()
-
+    #from CelestePy.util.plots import add_colorbar_to_axis
+    #fig, axarr = plt.subplots(1, 5, figsize=(16,4))
+    #for b, ax in zip(bands, axarr.flatten()):
+    #    cim = ax.imshow(imgdict[b], interpolation='none')
+    #    add_colorbar_to_axis(ax, cim)
+    #    ax.set_title("band %s"%b)
+    #fig.tight_layout()
+    #sys.exito
 
     #####################################################
     # create logpdf function handle (and gradient)      #
@@ -117,7 +126,6 @@ if __name__=="__main__":
                        psf_image_fixed_location_makers = model_img_fixed_funs)
     glnpdf = grad(lnpdf)
 
-
     #####################################################
     # find "map" (or best approx) and curvature at map  #
     #####################################################
@@ -126,21 +134,24 @@ if __name__=="__main__":
                    jac     = lambda th: -1.*grad(lnpdf)(th),
                    x0      = sample_from_prior(),
                    method  ='L-BFGS-B',
-                   options = {'maxiter':100, 'disp':10, 'ftol':1e-20})
+                   options = {'maxiter':10, 'disp':10, 'ftol':1e-10})
     th_map = res.x.copy()
-    H_map  = hessian(lnpdf)(th_map)
-    Sig    = np.linalg.inv(-H_map)
-    sig2   = np.diag(Sig)
+    colors_map, u_map, shape_map = th_map[:5], th_map[5:7], th_map[7:]
+    print "Map : th, sig, phi, rho", constrain_gal_shape(shape_map)
+    print "true: th, sig, phi, rho", true_shape
+    #H_map  = hessian(lnpdf)(th_map)
+    #Sig    = np.linalg.inv(-H_map)
+    #sig2   = np.diag(Sig)
 
     print "lnpdf value at map         ", lnpdf(th_map)
     print "lnpdf value at true params ", lnpdf(true_params)
 
     # at map, plot out random direction LNPDF values
-    from CelestePy.util.misc import eval_random_dir
-    fig = plt.figure(figsize=(12, 6))
-    for _ in xrange(5):
-        llz = eval_random_dir(th_map, lnpdf)
-        plt.plot(llz)
+    #from CelestePy.util.misc import eval_random_dir
+    #fig = plt.figure(figsize=(12, 6))
+    #for _ in xrange(2):
+    #    llz = eval_random_dir(th_map, lnpdf)
+    #    plt.plot(llz)
 
 
     ######################################################################
@@ -153,37 +164,37 @@ if __name__=="__main__":
     Nchains = 8
     th0s      = 1.1 * np.array([sample_from_prior() for _ in xrange(Nchains)])
     ll0s      = np.array([ lnpdf(th) for th in th0s ])
-    #mcmc_funs = [lambda th, llth: mhstep(th, lnpdf, llx=llth, prop_sig2=.15*sig2)
-    #             for _ in xrange(Nchains)]
-    #mcmc_funs = [lambda th, llth: slicesample(th, lnpdf, compwise=True)
-    #             for _ in xrange(Nchains)]
-    #th_samps, ll_samps = \
-    #    mcmc_multi_chain(th0s, ll0s, mcmc_funs, Nsamps=100, burnin=50)
-    #plot_chain_marginals(th_samps, true_params)
 
-    #######################################################################
-    # use the (should be faster) condition on u function to speed up 
-    # each chain
-    #######################################################################
     def gibbs_step(params, llth):
-        colors, u = params[0:5], params[5:7]
-        lnpdfu = lnpdf_u_maker(u)
+        """ steps between colors, u, shape """
+        colors, u, shape = params[0:5], params[5:7], params[7:]
+        lnpdfu = lnpdf_u_maker(u, shape)
         for _ in range(5):
             colors, _ = slicesample(colors, lnpdfu, compise=True)
-        u, ll     = slicesample(u, lambda u: lnpdf(np.concatenate([colors, u])), compwise=False)
-        return np.concatenate([colors, u]), ll
-    #th, llth = gibbs_step(true_params, lnpdf(true_params))
+        #print " ... colors "
+        lnpdf_loc = lambda u: lnpdf(np.concatenate([colors, u, shape]))
+        u, ll     = slicesample(u, lnpdf_loc, compwise=False)
+        #print " ... loc "
+        lnpdf_shp = lambda s: lnpdf(np.concatenate([colors, u, s]))
+        shape, ll = slicesample(shape, lnpdf_shp, compwise=False)
+        #print " ... shape "
+        return np.concatenate([colors, u, shape]), ll
+
     gibbs_funs = [gibbs_step for _ in xrange(Nchains)]
+    np.random.seed(42)
+    gibbs_step(true_params, lnpdf(true_params))
 
     th_samps, ll_samps = \
         mcmc_multi_chain(th0s, ll0s, gibbs_funs, Nsamps=500, burnin=100)
     _, Nsamps, D = th_samps.shape
 
     import cPickle as pickle
-    with open('synthetic_star_samps.pkl', 'wb') as f:
+    with open('synthetic_gal_samps.pkl', 'wb') as f:
         pickle.dump(th_samps, f)
         pickle.dump(ll_samps, f)
 
+
+    sys.exit
     fig, axarr = plt.subplots(3, 1, figsize=(12,4))
     axarr[0].plot(ll_samps[:, Nsamps/2:].T)
     axarr[0].set_title("log likelihood")
@@ -196,43 +207,4 @@ if __name__=="__main__":
     plot_chain_marginals(th_samps, true_params)
     plot_pairwise(th_flat, true_params)
 
-
-    ##################################################################
-    # MCMC with parallel tempering - construct multiple parallel     #
-    # tempering chains (using MH within) compare mixing              #
-    # do we end up with more effectively independent samples??       #
-    ##################################################################
-    #import CelestePy.util.infer.parallel_tempering as pt
-
-    ## create parrallel tempering step functions
-    #def make_pt_step():
-    #    num_temps = 12
-    #    def mh_step_maker(lnpdf):
-    #        #return lambda th, llth: mhstep(th, lnpdf, llx=llth, prop_sig2=.1*sig2)
-    #        return lambda th, llth: slicesample(th, lnpdf, compwise=False)
-
-    #    temp_th0s = np.array([sample_from_prior() for _ in xrange(num_temps)])
-    #    pt_step, pt_swaps = pt.make_parallel_tempering_sample_fun(
-    #                                th0s     = temp_th0s,
-    #                                lnpdf    = lnpdf,
-    #                                invtemps = np.linspace(.01, 1., num_temps),
-    #                                mcmc_step_maker = mh_step_maker)
-    #    return pt_step, pt_swaps
-
-    #pt_steps_and_swaps = [make_pt_step() for _ in range(Nchains)]
-    #pt_steps           = [p[0] for p in pt_steps_and_swaps]
-    #pt_swaps           = [p[1] for p in pt_steps_and_swaps]
-    #th0s = np.array([1.1*sample_from_prior() for _ in range(Nchains)])
-    #ll0s = np.array([lnpdf(th) for th in th0s])
-
-    #def callback(n):
-    #    print "swaps in chains: "
-    #    for pts in pt_swaps:
-    #        print pts()
-
-    #th_samps, ll_samps = \
-    #    mcmc_multi_chain(th0s, ll0s, pt_steps, Nsamps=200, burnin=100, callback=callback)
-
-    #plot_chain_marginals(th_samps, true_params)
-    #plot_pairwise(th_samps[0], true_params)
 
